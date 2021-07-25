e60d77214e22f5aca92e6771edf4001c59edc7c4
Dead-Fiend <abrikos12393@gmail.com>
2021-07-25 20:55:22 +0300

    upd

--- mini-app/src/index.js
+++ mini-app/src/index.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const bridge = require('@vkontakte/vk-bridge')
-let App = require('./App')
+const App = require('./App')
 //import React from "react";
 //import ReactDOM from "react-dom";
 //import bridge from "@vkontakte/vk-bridge";
@@ -10,7 +10,7 @@ let App = require('./App')
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(App, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
 }

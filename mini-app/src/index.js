24518173c60145aaf9f8693a4e48591fde722479
Dead-Fiend <abrikos12393@gmail.com>
2021-07-25 21:00:00 +0300

    upd

--- mini-app/src/index.js
+++ mini-app/src/index.js
@@ -1,11 +1,7 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const bridge = require('@vkontakte/vk-bridge')
-const App = require('./App')
-//import React from "react";
-//import ReactDOM from "react-dom";
-//import bridge from "@vkontakte/vk-bridge";
-//import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import bridge from "@vkontakte/vk-bridge";
+import App from "./App";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
